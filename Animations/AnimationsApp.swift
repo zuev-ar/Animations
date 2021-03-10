a637741efb00b430a581de9250f004506ea55f18
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-03-10 19:23:51 +0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Arkasha Zuev on 10.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
