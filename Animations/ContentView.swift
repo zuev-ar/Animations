e3d249304005462aebed7834b8bef09ae1809e17
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-05-12 21:09:47 +0500

    project 6, part 3

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    var letters = Array("Hello SwiftUI")
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count) { num in
+                Text(String(self.letters[num]))
+                    .padding(5)
+                    .font(.title)
+                    .background(self.enabled ? Color.blue : Color.red)
+                    .offset(self.dragAmount)
+                    .animation(Animation.default.delay(Double(num) / 20))
+            }
+        }
+        .gesture(
+            DragGesture()
+                .onChanged({ (value) in
+                    self.dragAmount = value.translation
+                })
+                .onEnded({ (_) in
+                    self.dragAmount = .zero
+                    self.enabled.toggle()
+                })
+        )
     }
 }
 
